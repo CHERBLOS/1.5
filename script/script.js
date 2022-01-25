f45e3c1b8442c9694cf0385e39c12fb7be1380c4
CHERBLOS <92993083+CHERBLOS@users.noreply.github.com>
2022-01-25 17:44:29 +0300

    up

--- script/script.js
+++ script/script.js
@@ -1,9 +1,7 @@
-let mediaQuerySize = window.screen.width;
-const buttonMore = document.querySelector('.brands__button')
 const minimizerList = document.querySelector('.brands__container');
-let swiper;
+const buttonMore = minimizerList.nextElementSibling;
 
-buttonMore.addEventListener('click', function(){
+buttonMore.addEventListener('click', function btnMore(){
   minimizerList.classList.toggle('brands__minimized');
   buttonMore.classList.toggle('brands__button--active')
 
@@ -14,24 +12,16 @@ buttonMore.addEventListener('click', function(){
   }
 });
 
-const swiperEnable = function(){
-  if (mediaQuerySize < 768){
+document.addEventListener('DOMContentLoaded', function load(){
+  if (window.screen.width < 768){
     const swiper = new Swiper('.swiper', {
-      slidesPerView: 1.25,
+      slidesPerView: 1.17,
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
         clickable: 'true',
       },
     });
-  } else if(swiper = undefined){
-    swiper.destroy();
   }
-};
+})
 
-
-window.addEventListener("resize", function(){
-  mediaQuerySize = window.screen.width;
-  swiperEnable();
-});
-swiperEnable();
